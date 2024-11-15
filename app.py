29d86df65d72d61dd5f71b7eb81f917ebc6b86d0
Beachboy43 <A01799111@tec.mx>
2024-11-15 20:15:50 +0000

    commit 1

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ import streamlit as st
 
 
 def main():
+    st.image("logo.png")
+
     st.title("HEALTHLINE 2.0")
+    st.divider()
+    st.subheader("Misión")
+    st.write("Proveer una solución tecnológica que simplifique y optimice la administración de medicamentos, apoyando a los cuidadores, enfermeros, y familiares en la gestión de la salud de los pacientes de manera segura, personalizada y eficiente.")
+    st.divider()
+
+    st.write("Visión")
+
+
+
+    st.write("Convertirse en una herramienta clave para mejorar la seguridad, confianza y calidad de vida en el sector salud, especialmente en entornos geriátricos y hospitalarios, al reducir errores de medicación y apoyar el bienestar físico y emocional del personal y los pacientes.")
+
+    st.divider()
+    col1,col2=st.columns([2,5])
+    with col1:
+        st.image("logo.png",caption="Omar Castañeda", width=150)
+
+    with col2:
+        st.write("lorem impsum")
+
 
     user_text = st.text_input("ESCRIBE ALGO:")
 
